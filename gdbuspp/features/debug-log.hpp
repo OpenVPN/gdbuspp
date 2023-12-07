c28c56e003960f8f2098049bb3399aabcf30d8f4
David Sommerseth <davids@openvpn.net>
2023-12-07 01:03:09 +0100

    debug: Send debug log to stderr instead of stdcout
    The stdout is captured by the test-fd-send test, which would get all the
    debug information in addition to the requested payload.  The end result
    is a failed test.  Instead use stderr instead for debug messages.  This
    is in general a better output for debug logging.
    
    Signed-off-by: David Sommerseth <davids@openvpn.net>

--- gdbuspp/features/debug-log.hpp
+++ gdbuspp/features/debug-log.hpp
@@ -21,7 +21,7 @@
 
 #include <iostream>
 
-#define GDBUSPP_LOG(msg) std::cout << "[GDBus++ DEBUG (pid:" << getpid() << ") "                     \
+#define GDBUSPP_LOG(msg) std::cerr << "[GDBus++ DEBUG (pid:" << getpid() << ") "                     \
                                    << "{" << __FILE__ << ":" << __LINE__ << " " << __func__ << "}] " \
                                    << msg << std::endl;
 
