499e10f710bf8539a5cbbdd96ca692406a0865c4
David Sommerseth <dazo@eurephia.org>
2026-02-05 10:44:21 +0100

    mainloop: Replace _private namespace with an anonymous one
    Using an anonymous namespace for isolating local variables is just as
    efficient and gives less code clutter.
    
    Signed-off-by: David Sommerseth <dazo@eurephia.org>

--- gdbuspp/mainloop.cpp
+++ gdbuspp/mainloop.cpp
@@ -24,20 +24,17 @@
 namespace DBus {
 
 /**
- *  This namespace is only used for holding library internal data.
- *
- *  The intention is to avoid hiding information useful while debugging,
- *  but at the same time keep it in a separate namespace indicating it
- *  is not to be directly exposed to the users of the library.
+ *  This anonymous namespace is only used for
+ *  holding library internal data.
  */
-namespace _private {
+namespace {
 
 /**
  *  Internal mainloop object.  One process can only have one active
  *  mainloop
  */
 GMainLoop *_int_glib2_mainloop = nullptr;
-} // namespace _private
+} // namespace
 
 
 MainLoop::Exception::Exception(const std::string &errm)
@@ -48,42 +45,42 @@ MainLoop::Exception::Exception(const std::string &errm)
 
 void MainLoop::Run()
 {
-    if (_private::_int_glib2_mainloop)
+    if (_int_glib2_mainloop)
     {
         throw MainLoop::Exception("A main loop is already running");
     }
-    _private::_int_glib2_mainloop = g_main_loop_new(nullptr, false);
-    g_unix_signal_add(SIGINT, glib2::Callbacks::_int_mainloop_stop_handler, _private::_int_glib2_mainloop);
-    g_unix_signal_add(SIGTERM, glib2::Callbacks::_int_mainloop_stop_handler, _private::_int_glib2_mainloop);
-    g_main_loop_run(_private::_int_glib2_mainloop);
-    g_main_loop_unref(_private::_int_glib2_mainloop);
-    _private::_int_glib2_mainloop = nullptr;
+    _int_glib2_mainloop = g_main_loop_new(nullptr, false);
+    g_unix_signal_add(SIGINT, glib2::Callbacks::_int_mainloop_stop_handler, _int_glib2_mainloop);
+    g_unix_signal_add(SIGTERM, glib2::Callbacks::_int_mainloop_stop_handler, _int_glib2_mainloop);
+    g_main_loop_run(_int_glib2_mainloop);
+    g_main_loop_unref(_int_glib2_mainloop);
+    _int_glib2_mainloop = nullptr;
 }
 
 
 void MainLoop::Wait()
 {
-    if (_private::_int_glib2_mainloop)
+    if (_int_glib2_mainloop)
     {
-        g_main_loop_run(_private::_int_glib2_mainloop);
+        g_main_loop_run(_int_glib2_mainloop);
     }
 }
 
 
 bool MainLoop::Running() const
 {
-    return _private::_int_glib2_mainloop != nullptr;
+    return _int_glib2_mainloop != nullptr;
 }
 
 
 void MainLoop::Stop()
 {
-    if (!_private::_int_glib2_mainloop)
+    if (!_int_glib2_mainloop)
     {
         throw MainLoop::Exception("No main loop is running");
     }
 
-    g_main_loop_quit(_private::_int_glib2_mainloop);
+    g_main_loop_quit(_int_glib2_mainloop);
 }
 
 
